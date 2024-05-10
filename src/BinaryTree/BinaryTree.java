69e71647cdda49bdea083bf1687cded537c48095
João Paulo Dias Estevão <joao.diasest@gmail.com>
2024-05-10 01:30:06 -0300

    ex3

--- src/BinaryTree/BinaryTree.java
+++ src/BinaryTree/BinaryTree.java
@@ -2,34 +2,56 @@ package BinaryTree;
 
 public class BinaryTree {
 	Node root;
-	public BinaryTree() { root = null; }
-	
-	public void inserir(int n) throws Exception{
+
+	public BinaryTree() {
+		root = null;
+	}
+
+	public void inserir(int n) throws Exception {
 		root = inserir(n, root);
 	}
-	
-	Node inserir(int n, Node node) throws Exception{
-		if(node == null) 
+
+	Node inserir(int n, Node node) throws Exception {
+		if (node == null)
 			node = new Node(n);
-		
-		else if(n < node.element) 
+
+		else if (n < node.element)
 			node.esq = inserir(n, node.esq);
-		
-		else if(n > node.element)
+
+		else if (n > node.element)
 			node.dir = inserir(n, node.dir);
-		
+
 		else
 			throw new Exception("Nao eh possivel inserir elementos repetidos");
-		
+
 		return node;
 	}
-	
+
+	public boolean pesquisar(int n) {
+		return pesquisar(n, root);
+	}
+
+	boolean pesquisar(int n, Node node) {
+		boolean resp;
+		if (node == null) {
+			resp = false;
+		} else if (n < node.element) {
+			resp = pesquisar(n, node.esq);
+		} else if (n > node.element) {
+			resp = pesquisar(n, node.dir);
+		} else {
+			resp = true;
+		}
+
+		return resp;
+	}
+
 	public void caminharCentral() {
 		System.out.print("[ ");
 		caminharCentral(root);
 		System.out.print("]");
 	}
-	
+
 	void caminharCentral(Node node) {
 		if (node != null) {
 			caminharCentral(node.esq);
@@ -37,13 +59,13 @@ public class BinaryTree {
 			caminharCentral(node.dir);
 		}
 	}
-	
+
 	public void caminharPreOrdem() {
 		System.out.print("[ ");
 		caminharPreOrdem(root);
 		System.out.print("]");
 	}
-	
+
 	void caminharPreOrdem(Node node) {
 		if (node != null) {
 			System.out.print(node.element + " ");
@@ -51,18 +73,34 @@ public class BinaryTree {
 			caminharPreOrdem(node.dir);
 		}
 	}
+
 	public void caminharPosOrdem() {
 		System.out.print("[ ");
 		caminharPosOrdem(root);
 		System.out.print("]");
 	}
-	
+
 	void caminharPosOrdem(Node node) {
 		if (node != null) {
 			caminharPosOrdem(node.esq);
 			caminharPosOrdem(node.dir);
 			System.out.print(node.element + " ");
 		}
+	}
+
+	public int getAltura() {
+		return getAltura(root);
+	}
+
+	private int getAltura(Node node) {
+		if (node == null) {
+			return -1;
+		} else {
+			int alturaEsq = getAltura(node.esq);
+			int alturaDir = getAltura(node.dir);
+			return Math.max(alturaEsq, alturaDir) + 1;
+		}
+
 	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 import BinaryTree.BinaryTree;
 
 public class Main {
@@ -6,16 +8,34 @@ public class Main {
 		Pilha pilha = new Pilha();
 		LinkedList list = new LinkedList();
 		BinaryTree tree = new BinaryTree();
-		
-		tree.inserir(7);
-		tree.inserir(2);
-		tree.inserir(0);
-		tree.inserir(12);
 
-		tree.caminharCentral();
-		tree.caminharPreOrdem();
+		// tree.caminharCentral();
+		// tree.caminharPreOrdem();
+		// tree.caminharPosOrdem();
+
+		// System.out.println(tree.getAltura());
+
+		Random rand = new Random();
+		rand.setSeed(0);
+
+		for (int i = 0; i <= 100; i++) {
+			int val;
+
+			do {
+				val = Math.abs(rand.nextInt());
+			} while (tree.pesquisar(val) == true);
+			
+			tree.inserir(val);
+			
+			if(i % 1000 == 0) {
+				double lg = (Math.log(i) / Math.log(2));
+				lg *= 1.39;
+				System.out.println("Numero de nos: " + i + " --- log(i,2) = " + lg + " --- h = " + tree.getAltura());
+
+			}
+			
+		}
 		tree.caminharPosOrdem();
-		
 	}
 
 }
